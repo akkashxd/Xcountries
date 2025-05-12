29169b725bd96e53ef284cfed7614dab0579b604
akkashxd <yobatlu@gmail.com>
2025-05-12 14:10:29 +0530

    test

--- xcountries/src/App.js
+++ xcountries/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("API error");
         }
         return res.json();
       })
@@ -17,55 +17,44 @@ export default function App() {
         setCountries(data);
         setIsLoading(false);
       })
-      .catch((err) => {
-        setError("Failed to load countries");
+      .catch(() => {
+        setError("Something went wrong");
         setIsLoading(false);
       });
   }, []);
 
-  const cardStyle = {
-    width: "200px",
-    border: "1px solid #ccc",
-    borderRadius: "10px",
-    margin: "10px",
-    padding: "10px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const imageStyle = {
-    width: "100px",
-    height: "100px",
-  };
-
-  const containerStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    minHeight: "100vh",
-  };
-
-  // Show loading message
   if (isLoading) {
-    return <div style={{ textAlign: "center", marginTop: "2rem" }}>Loading...</div>;
+    return <div>Loading...</div>;
   }
 
-  // Show error message
   if (error) {
-    return <div style={{ textAlign: "center", color: "red", marginTop: "2rem" }}>{error}</div>;
+    return <div>{error}</div>;
   }
 
   return (
-    <div style={containerStyle}>
+    <div style={{
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh"
+    }}>
       {countries.map((country) => (
-        <div key={country.cca3} style={cardStyle}>
+        <div key={country.cca3} style={{
+          width: "200px",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
+          margin: "10px",
+          padding: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
           <img
             src={country.flags.png}
             alt={`Flag of ${country.name.common}`}
-            style={imageStyle}
+            style={{ width: "100px", height: "100px" }}
           />
           <h2>{country.name.common}</h2>
         </div>
